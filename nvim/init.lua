9b3d0a44413aa84a5cee5cc2895300b02a23c4ae
Jonny Johannes <jonny@jonnyjohannes.com>
2025-02-02 17:59:44 -0500

    lualine setup with tpipeline

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,6 +39,7 @@ require('lazy').setup({
     { 'nvim-treesitter/nvim-treesitter' },
     { 'sheerun/vim-polyglot' },
     { 'tpope/vim-fugitive' },
+    { 'vimpostor/vim-tpipeline' },
 })
 
 -- color scheme
@@ -53,9 +54,9 @@ local fzfLua = require('fzf-lua')
 fzfLua.setup({
   "borderless_full",
 })
-vim.keymap.set('n', '<Leader>t', fzfLua.files, { noremap = true, silent = true })
-vim.keymap.set('n', '<Leader>f', fzfLua.grep_project, { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>b', fzfLua.buffers, { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>f', fzfLua.grep_project, { noremap = true, silent = true })
+vim.keymap.set('n', '<Leader>t', fzfLua.files, { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>x', fzfLua.command_history, { noremap = true, silent = true })
 vim.keymap.set('n', '<Leader>/', fzfLua.search_history, { noremap = true, silent = true })
 
@@ -79,48 +80,18 @@ vim.keymap.set('n', '<leader><leader>j', smartSplits.swap_buf_down)
 vim.keymap.set('n', '<leader><leader>k', smartSplits.swap_buf_up)
 vim.keymap.set('n', '<leader><leader>l', smartSplits.swap_buf_right)
 
--- lualine
+-- lualine + tpipeline
+vim.cmd('let g:tpipeline_autoembed = 0')
+vim.cmd('autocmd BufNew * set laststatus=0 showtabline=0 noshowmode noruler noshowcmd')
+
 require('lualine').setup({
   options = {
     theme = 'monokai-pro',
     section_separators = '',
     component_separators = '',
+    globalstatus = true,
   },
   sections = {
-    lualine_a = {
-    },
-    lualine_b = {
-      'mode'
-    },
-    lualine_c = {
-      'filename'
-    },
-    lualine_x = {
-      'filetype'
-    },
-    lualine_y = {
-      'location'
-    },
-    lualine_z = {
-    },
-  },
-  inactive_sections = {
-    lualine_a = {
-    },
-    lualine_b = {
-    },
-    lualine_c = {
-      'filename'
-    },
-    lualine_x = {
-      'filetype'
-    },
-    lualine_y = {
-    },
-    lualine_z = {
-    },
-  },
-  tabline = {
     lualine_a = {
       {
         'buffers',
@@ -133,13 +104,19 @@ require('lualine').setup({
     lualine_c = {
     },
     lualine_x = {
+      'filetype',
     },
     lualine_y = {
-      'branch'
+      {
+        'FugitiveHead',
+        icon = '',
+      }
     },
     lualine_z = {
+      'mode',
     },
   },
+  inactive_sections = { },
 })
 
 -- alpha
@@ -152,6 +129,7 @@ local header = {
   '    ██╔══██║██║   ██║       ██║   ██║       ',  
   '    ██║  ██║██║   ██║       ██║   ██║       ',  
   '    ╚═╝  ╚═╝╚═╝   ╚═╝       ╚═╝   ╚═╝       ',  
+  '                 ¯\\(°_o)/¯                 ',
 }
 dashboard.section.header.val = header
 dashboard.section.buttons.val = {
@@ -165,7 +143,7 @@ dashboard.section.buttons.val = {
 alpha.setup(dashboard.opts)
 
 -- treesitter
-require'nvim-treesitter.configs'.setup({
+require('nvim-treesitter.configs').setup({
   ensure_installed = {
     "java",
     "javascript",
