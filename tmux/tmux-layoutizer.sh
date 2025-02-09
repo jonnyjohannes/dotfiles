f3c21f3547a6aaa646e22d731ffed106d2131176
Jonny Johannes <jonny@jonnyjohannes.com>
2025-02-09 00:01:45 -0500

    tmux-layoutizer

---
+++ tmux/tmux-layoutizer.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Inspired by ThePrimegean's tmux-sessionizer and tmux-windowizer
+
+selected=$(find ~/src -mindepth 2 -maxdepth 2 -type d | cut -d'/' -f5- | fzf --tmux --border sharp --prompt=" ")
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+dir="$HOME/src/$selected"
+
+session=$(echo $selected | cut -d'/' -f1 | tr . _)
+window=$(echo $selected | cut -d'/' -f2 | tr . _)
+target="$session:$window"
+
+tmux_running=$(pgrep tmux)
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $session
+    exit 0
+fi
+
+create_tmux_layout() {
+    local window=$1
+    local dir=$2
+
+    tmux select-window -t $window
+    tmux split-window -h -p 70 -c $dir
+    tmux select-pane -L
+    tmux split-window -v -p 30 -c $dir
+    tmux select-pane -U
+    tmux select-pane -R
+}
+
+if ! tmux has-session -t $session 2> /dev/null; then
+    tmux new-session -ds $session -n $window
+    create_tmux_layout $window $dir
+fi
+
+if ! tmux has-session -t $target 2> /dev/null; then
+    tmux new-window -dn $window -c $dir
+    create_tmux_layout $window $dir
+fi
+
+tmux switch-client -t $session
+tmux select-window -t $target
+
