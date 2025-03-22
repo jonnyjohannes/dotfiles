d25da81b5cd78e5715520465ed1abc423897efda
Jonny Johannes <jonny@jonnyjohannes.com>
2025-03-22 13:04:55 -0400

    mason to manage the lsps and daps

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,19 +34,24 @@ require('lazy').setup({
     },
     { 'gelguy/wilder.nvim' },
     { 'github/copilot.vim' },
-    { 'ibhagwan/fzf-lua' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'ibhagwan/fzf-lua', commit = 'ed8761eaa58ff77312a877a4de63d016d58dfc58' },
     { 'loctvl842/monokai-pro.nvim' },
     { 'mfussenegger/nvim-dap' },
-    { 'mfussenegger/nvim-dap-python' },
-    { 'mfussenegger/nvim-jdtls' },
     { 'mrjones2014/smart-splits.nvim' },
     { 'neovim/nvim-lspconfig' },
     { 'nvim-lualine/lualine.nvim' },
     { 'nvim-tree/nvim-tree.lua' },
     { 'nvim-tree/nvim-web-devicons' },
     { 'nvim-treesitter/nvim-treesitter' },
+    { 'rose-pine/neovim' },
     { 'sheerun/vim-polyglot' },
     { 'tpope/vim-fugitive' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 })
 
 -- color scheme
@@ -179,5 +184,112 @@ wilder.set_option('pipeline', {
       fuzzy = 2,
     })
   )
+})
+
+-- LSP and completion
+local cmp = require('cmp')
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- nvim-cmp setup
+cmp.setup({
+  completion = {
+    autocomplete = false,
+  },
+  snippet = {
+    expand = function(args)
+      -- You'll need a snippet engine - add one to your dependencies if needed
+      -- e.g., luasnip: require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
+})
+
+mason.setup({})
+
+local dap = require('dap')
+dap.adapters.python = {
+  type = 'executable',
+  command = '/Users/jj678a/.pyenv/versions/novibedj/bin/python',
+  args = { '-m', 'debugpy.adapter' },
+}
+dap.configurations.python = {
+  {
+    type = 'python',
+    request = 'launch',
+    name = 'Launch file',
+    program = "${file}",
+    console = "integratedTerminal",
+    justMyCode = false,
+    pythonPath = function()
+      if vim.env.VIRTUAL_ENV then
+        return vim.env.VIRTUAL_ENV .. '/bin/python'
+      end
+      return '/Users/jj678a/.pyenv/versions/novibedj/bin/python'
+    end,
+  },
+}
+
+-- vim.api.nvim_create_autocmd({"FileType"}, {
+--   pattern = {"python"},
+--   callback = function()
+--     -- Check for project-specific .nvim/dap.lua file
+--     local project_config = vim.fn.getcwd() .. "/.dap.lua"
+--     if vim.fn.filereadable(project_config) == 1 then
+--       -- Create a unique module name for this project's config
+--       local module_name = "project_dap_" .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+--       -- Add the directory to package.path temporarily
+--       package.loaded[module_name] = nil
+--       local old_path = package.path
+--       package.path = vim.fn.getcwd() .. "/?.lua;" .. package.path
+--       -- Require the module
+--       pcall(require, module_name)
+--       -- Restore package.path
+--       package.path = old_path
+--     end
+--   end
+-- })
+
+mason_lspconfig.setup({})
+mason_lspconfig.setup_handlers({
+  function(server_name)
+    require('lspconfig')[server_name].setup({
+    capabilities = capabilities,
+    on_attach = function()
+      vim.keymap.set("n", "<leader>gd", fzfLua.lsp_definitions)
+      vim.keymap.set("n", "<leader>gr", fzfLua.lsp_references)
+
+      -- dap mappings
+      vim.keymap.set("n", "<leader>db", require('dap').toggle_breakpoint)
+      vim.keymap.set("n", "<leader>dc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>")
+    end,
+  })
+  end,
 })
 
