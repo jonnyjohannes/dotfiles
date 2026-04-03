2a94093ddd18656a7266f6289f0f9591a5c49853
Jonny Johannes <jonny@jonnyjohannes.com>
2026-04-02 22:07:27 -0400

    disable molten/jupytext, update plugin versions

--- nvim/lua/plugins/molten.lua
+++ nvim/lua/plugins/molten.lua
@@ -1,27 +1,27 @@
-local vim = vim
+-- local vim = vim
 return {
-  {
-    'goerz/jupytext.nvim',
-    version = '0.2.0',
-    opts = {},
-  },
-  {
-    'benlubas/molten-nvim',
-    dependencies = {
-      '3rd/image.nvim'
-    },
-    config = function()
-      vim.g.molten_image_provider = 'image.nvim'
-
-      vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-        pattern = '*.ipynb',
-        callback = function()
-          vim.keymap.set('n', '<M-r>', ':MoltenReevaluateCell<CR>')
-          vim.keymap.set('v', '<M-r>', ':<C-u>MoltenEvaluateVisual<CR>gv')
-          vim.keymap.set('n', '<M-e>', ':noautocmd MoltenEnterOutput<CR>')
-        end,
-      })
-    end,
-    build = ':UpdateRemotePlugins',
-  }
+--   {
+--     'goerz/jupytext.nvim',
+--     version = '0.2.0',
+--     opts = {},
+--   },
+--   {
+--     'benlubas/molten-nvim',
+--     dependencies = {
+--       '3rd/image.nvim'
+--     },
+--     config = function()
+--       vim.g.molten_image_provider = 'image.nvim'
+--
+--       vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+--         pattern = '*.ipynb',
+--         callback = function()
+--           vim.keymap.set('n', '<M-r>', ':MoltenReevaluateCell<CR>')
+--           vim.keymap.set('v', '<M-r>', ':<C-u>MoltenEvaluateVisual<CR>gv')
+--           vim.keymap.set('n', '<M-e>', ':noautocmd MoltenEnterOutput<CR>')
+--         end,
+--       })
+--     end,
+--     build = ':UpdateRemotePlugins',
+--   }
 }
