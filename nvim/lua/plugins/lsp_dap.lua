fb90848a4545231b9ba9146a0979d5aa8aeb8617
Jonny Johannes <jonny@jonnyjohannes.com>
2026-04-02 22:07:29 -0400

    add markdown prettier formatter to conform

--- nvim/lua/plugins/lsp_dap.lua
+++ nvim/lua/plugins/lsp_dap.lua
@@ -81,6 +81,7 @@ return {
     config = function()
       require('conform').setup({
         formatters_by_ft = {
+          markdown = { 'prettier' },
           python = { 'black' },
           sh = { 'beautysh' },
           json = { 'jq' },
