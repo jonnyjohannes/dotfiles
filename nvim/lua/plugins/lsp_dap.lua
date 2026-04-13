ac24dd85cc576597efcea76e1b50d950a0c23b28
Jonny Johannes <jonny@jonnyjohannes.com>
2026-04-13 09:07:41 -0400

    refactor lsp setup to wildcard config + LspAttach autocmd
    <|°_°|> [opus 4.6]

--- nvim/lua/plugins/lsp_dap.lua
+++ nvim/lua/plugins/lsp_dap.lua
@@ -26,30 +26,30 @@ return {
         dofile(project_j2de_path)
       end
 
-      local mason_lspconfig = require('mason-lspconfig')
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
-        vim.lsp.config[server_name] = {
-          capabilities = capabilities,
-          on_attach = function()
-            vim.keymap.set('n', 'gd', require('fzf-lua').lsp_definitions)
-            vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references)
-            vim.keymap.set('n', '<M-R>', require('dap').continue)
-            vim.keymap.set('n', '<M-r>', require('dap').step_over)
-            vim.keymap.set('n', '<M-e>', require('dap').step_into)
-            vim.keymap.set('n', '<M-w>', require('dap').step_out)
-            vim.keymap.set('n', '<M-x>', require('dap').terminate)
-            vim.keymap.set('n', '<leader>db', require('dap').toggle_breakpoint)
-            vim.keymap.set('n', '<leader>dB', function()
-              vim.ui.input({ prompt = 'Breakpoint condition: ' }, function(cond)
-                require('dap').set_breakpoint(cond)
-              end)
+      require('mason-lspconfig').setup()
+
+      vim.lsp.config('*', {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+      })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function()
+          vim.keymap.set('n', 'gd', require('fzf-lua').lsp_definitions)
+          vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references)
+          vim.keymap.set('n', '<M-R>', require('dap').continue)
+          vim.keymap.set('n', '<M-r>', require('dap').step_over)
+          vim.keymap.set('n', '<M-e>', require('dap').step_into)
+          vim.keymap.set('n', '<M-w>', require('dap').step_out)
+          vim.keymap.set('n', '<M-x>', require('dap').terminate)
+          vim.keymap.set('n', '<leader>db', require('dap').toggle_breakpoint)
+          vim.keymap.set('n', '<leader>dB', function()
+            vim.ui.input({ prompt = 'Breakpoint condition: ' }, function(cond)
+              require('dap').set_breakpoint(cond)
             end)
-            vim.keymap.set('n', '<leader>dr', require('dap-view').toggle)
-          end,
-        }
-        vim.lsp.enable(server_name)
-      end
+          end)
+          vim.keymap.set('n', '<leader>dr', require('dap-view').toggle)
+        end,
+      })
 
       vim.keymap.set('n', 'K', function()
         vim.lsp.buf.hover({ border = 'single' })
