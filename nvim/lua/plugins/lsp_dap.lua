0bd287762f8cd7f4f5bd0f040d7d3940844f28fc
Jonny Johannes <jonny@jonnyjohannes.com>
2026-07-29 21:18:33 -0400

    clean up nvim plugin config <|°_°|>

--- nvim/lua/plugins/lsp_dap.lua
+++ nvim/lua/plugins/lsp_dap.lua
@@ -34,6 +34,8 @@ return {
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function()
+          vim.keymap.set('n', 'gd', require('fzf-lua').lsp_definitions)
+          vim.keymap.set('n', 'gr', require('fzf-lua').lsp_references)
           vim.keymap.set('n', '<M-R>', require('dap').continue)
           vim.keymap.set('n', '<M-r>', require('dap').step_over)
           vim.keymap.set('n', '<M-e>', require('dap').step_into)
